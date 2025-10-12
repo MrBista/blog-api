064b8343f1a51623694ee3c4e151360a2add9861
MrBista <gustibisman2308@gmail.com>
2025-10-12 08:52:21 +0700

    category crud final

--- internal/handler/category.go
+++ internal/handler/category.go
@@ -1,7 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/MrBista/blog-api/internal/dto"
+	"github.com/MrBista/blog-api/internal/exception"
 	"github.com/MrBista/blog-api/internal/services"
+	"github.com/MrBista/blog-api/internal/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,6 +16,7 @@ type CategoryHandler interface {
 	FindCategoryById(c *fiber.Ctx) error
 	CreateCategory(c *fiber.Ctx) error
 	UpdateCategory(c *fiber.Ctx) error
+	DeleteCategory(c *fiber.Ctx) error
 }
 
 type CategoryHandlerImpl struct {
@@ -23,17 +30,118 @@ func NewCategoryHandler(categoryService services.CategoryService) CategoryHandle
 }
 
 func (h *CategoryHandlerImpl) FindAllCategory(c *fiber.Ctx) error {
-	panic("not implemented") // TODO: Implement
+
+	categoriesRes, err := h.CategoryService.FindAllCategory()
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.CommonResponseSuccess{
+		Data:    categoriesRes,
+		Status:  fiber.StatusOK,
+		Message: "Successfully get all category",
+	})
 }
 
 func (h *CategoryHandlerImpl) FindCategoryById(c *fiber.Ctx) error {
-	panic("not implemented") // TODO: Implement
+	// panic("not implemented") // TODO: Implement
+
+	paramId := c.Params("id")
+
+	val, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return exception.NewBadRequestErr(err.Error())
+	}
+
+	categoryDetail, err := h.CategoryService.FindById(val)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.CommonResponseSuccess{
+		Data:    categoryDetail,
+		Message: "Successfully get detail category",
+		Status:  fiber.StatusOK,
+	})
 }
 
 func (h *CategoryHandlerImpl) CreateCategory(c *fiber.Ctx) error {
-	panic("not implemented") // TODO: Implement
+	var categoryReqbody dto.CategoryRequst
+
+	body := c.Body()
+
+	if err := json.Unmarshal(body, &categoryReqbody); err != nil {
+		return err
+	}
+
+	validator := utils.GetValidator()
+
+	if err := validator.Struct(categoryReqbody); err != nil {
+		return exception.NewValidationErr(err)
+	}
+
+	err := h.CategoryService.CreateCategory(categoryReqbody)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(dto.CommonResponseSuccess{
+		Data:    true,
+		Message: "Successfully create category",
+		Status:  fiber.StatusCreated,
+	})
 }
 
 func (h *CategoryHandlerImpl) UpdateCategory(c *fiber.Ctx) error {
-	panic("not implemented") // TODO: Implement
+	var categoryReqbody dto.CategoryRequst
+
+	body := c.Body()
+
+	if err := json.Unmarshal(body, &categoryReqbody); err != nil {
+		return err
+	}
+
+	paramId := c.Params("id")
+	valId, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return exception.NewBadRequestErr(err.Error())
+	}
+
+	err = h.CategoryService.UpdateCategory(valId, categoryReqbody)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.CommonResponseSuccess{
+		Data:    true,
+		Message: "Successfully update category",
+		Status:  fiber.StatusOK,
+	})
+}
+
+func (h *CategoryHandlerImpl) DeleteCategory(c *fiber.Ctx) error {
+
+	paramId := c.Params("id")
+	valId, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return exception.NewBadRequestErr(err.Error())
+	}
+
+	err = h.CategoryService.DeleteById(valId)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.CommonResponseSuccess{
+		Data:    true,
+		Message: "Successfully delete category",
+		Status:  fiber.StatusOK,
+	})
 }
--- internal/router/category.go
+++ internal/router/category.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/MrBista/blog-api/internal/handler"
+	"github.com/MrBista/blog-api/internal/middleware"
 	"github.com/MrBista/blog-api/internal/repository"
 	"github.com/MrBista/blog-api/internal/services"
 	"github.com/gofiber/fiber/v2"
@@ -15,8 +16,10 @@ func SetupCategoryRouter(route fiber.Router, db *gorm.DB) {
 
 	categoryRouter := route.Group("/categories")
 
+	categoryRouter.Get("/:id", middleware.AuthMiddlware(), categoryHandler.FindCategoryById)
+	categoryRouter.Put("/:id", middleware.AuthMiddlware(), categoryHandler.UpdateCategory)
+	categoryRouter.Delete("/:id", middleware.AuthMiddlware(), categoryHandler.DeleteCategory)
 	categoryRouter.Get("/", categoryHandler.FindAllCategory)
-	categoryRouter.Get("/:id", categoryHandler.FindCategoryById)
-	categoryRouter.Post("/", categoryHandler.CreateCategory)
+	categoryRouter.Post("/", middleware.AuthMiddlware(), categoryHandler.CreateCategory)
 
 }
